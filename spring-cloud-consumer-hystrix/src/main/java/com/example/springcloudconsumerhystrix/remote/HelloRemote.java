44b53a29dea4146eac89b2af815ea72539a102ee
wangxinjie <wangxinjie@iboxpay.com>
2018-08-23 15:09:30 +0800

    first commit

---
+++ spring-cloud-consumer-hystrix/src/main/java/com/example/springcloudconsumerhystrix/remote/HelloRemote.java
@@ -0,0 +1,16 @@
+package com.example.springcloudconsumerhystrix.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by summer on 2017/5/11.
+ */
+@FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+
+}
